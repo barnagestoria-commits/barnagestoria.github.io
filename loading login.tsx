25ea88a91bc895515bfc4225272dbb9b5db13a3f
barnagestoria-commits <barnagestoria@gmail.com>
2025-08-28 03:15:17 +0200

    Create loading login.tsx

---
+++ loading login.tsx
@@ -0,0 +1,5 @@
+import { SeasonalPageLoading } from "@/components/seasonal-page-loading"
+
+export default function Loading() {
+  return <SeasonalPageLoading message="Preparando inicio de sesión..." size="sm" />
+}
